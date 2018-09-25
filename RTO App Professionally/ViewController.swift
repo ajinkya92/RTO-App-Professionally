7c64b98af3277755890ecb3dfc9eb1293432c9ba
Ajinkya Sonar <Ajinkya@ajinkyas-macbook-pro.local>
2018-09-25 18:06:45 +0530

    Initial Commit

---
+++ RTO App Professionally/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  RTO App Professionally
+//
+//  Created by Ajinkya Sonar on 25/09/18.
+//  Copyright © 2018 Ajinkya Sonar. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
